6a2a31be17f0fd0327ee56261e95cc66629d4501
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-09-12 12:10:54 -0300

    fix: parte de operadores finalizada

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,31 +1,13 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
-        int numero2 = 5;
+        int numero1 = 1;
+        int numero2 = 2;
 
-        int somar = somar(numero, numero2);
-        System.out.println(somar);
+        numero1++;
+        System.out.println(numero1);
 
-        int subtrair = subtrair(numero, numero2);
-        System.out.println(subtrair);
+        boolean simNao = numero1 <= numero2;
+        System.out.println(simNao);
 
-        int multiplicar = multiplicar(numero, numero2);
-        System.out.println(multiplicar);
-
-        int dividir = dividir(numero, numero2);
-        System.out.println(dividir);
-
-    }
-    public static int somar(int numero, int numero2){
-        return numero + numero2;
-    }
-    public static int subtrair(int numero, int numero2){
-        return numero - numero2;
-    }
-    public static int multiplicar(int numero, int numero2){
-        return numero * numero2;
-    }
-    public static int dividir(int numero, int numero2){
-        return numero / numero2;
     }
 }
